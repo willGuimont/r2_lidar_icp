5719e284b84cd63dec76582efbc9dba74409b7c0
William Guimont-Martin <william.guimont-martin.1@ulaval.ca>
2021-10-28 23:20:48 -0400

    lidar visualization

--- lidar.py
+++ lidar.py
@@ -1,33 +1,35 @@
-import matplotlib.pyplot as plt
+import cv2.cv2 as cv2
 import numpy as np
 from rplidar import RPLidar
 
 lidar = RPLidar('/dev/ttyUSB0')
+window = "lidar"
+window_size = 500
 
-info = lidar.get_info()
-print(info)
-
-health = lidar.get_health()
-print(health)
-
-
-def plot_scan(scan):
-    quality, angle, distance = scan[:, 0], scan[:, 1], scan[:, 2]
-    angle = np.deg2rad(angle)
-
-    fig, ax = plt.subplots(subplot_kw={'projection': 'polar'})
-    ax.scatter(angle, distance)
-    ax.set_rlabel_position(-22.5)
-    ax.grid(True)
-
-    ax.set_title('LiDAR')
-    plt.show()
-
+cv2.namedWindow(window)
 
 for i, scan in enumerate(lidar.iter_scans()):
+    img = np.zeros((window_size, window_size, 3), dtype=np.uint8)
     scan = np.array(scan)
-    plot_scan(scan)
-    if i > 10:
+
+    qualities, angles, distances = scan[:, 0], scan[:, 1], scan[:, 2]
+    angles = np.deg2rad(angles)
+
+    xs = np.cos(angles) * distances
+    ys = np.sin(angles) * distances
+
+    for (x, y) in zip(xs, ys):
+        x = int(x / 6500 * window_size)
+        y = int(y / 6500 * window_size)
+
+        x += window_size // 2
+        y += window_size // 2
+
+        cv2.circle(img, (x, y), 5, (255, 255, 0), -1)
+
+    cv2.imshow(window, img)
+    key = cv2.waitKey(1)
+    if key == ord('q') or key == 27:
         break
 
 lidar.stop()
