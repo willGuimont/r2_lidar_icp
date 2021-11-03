e3715e49584bb64c45dc821a0438218599ddcb04
William Guimont-Martin <wil9876543210@hotmail.com>
2021-11-03 19:57:18 -0400

    save lidar

---
+++ replay_scan.py
@@ -0,0 +1,42 @@
+import pathlib
+import pickle
+
+import cv2.cv2 as cv2
+import numpy as np
+import argparse
+
+from visualize_point_cloud import draw_point_cloud
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Replay a lidar scan sequence')
+    parser.add_argument('scans_path', help='Scan folder')
+    args = parser.parse_args()
+
+    scans_path = pathlib.Path(args.scans_path)
+    scans_paths = scans_path.iterdir()
+
+    window = "lidar"
+    window_size = 500
+
+    cv2.namedWindow(window)
+    for i, scan in enumerate(sorted(list(scans_paths))):
+        img = np.zeros((window_size, window_size, 3), dtype=np.uint8)
+        scan = pickle.load(open(scan, 'rb'))
+
+        qualities, angles, distances = scan[:, 0], scan[:, 1], scan[:, 2]
+        angles = np.deg2rad(angles)
+
+        xs = np.cos(angles) * distances
+        ys = np.sin(angles) * distances
+
+        pc = np.stack((xs, ys))
+        draw_point_cloud(pc, img, window_size, (255, 255, 0))
+
+        cv2.imshow(window, img)
+        key = cv2.waitKey(100)
+        if key == ord(' '):
+            pickle.dump(scan, open(f'scans/{i}.pkl', 'wb'))
+        if key == ord('q') or key == 27:
+            break
+
+    cv2.destroyAllWindows()
---
+++ save_lidar.py
@@ -0,0 +1,30 @@
+import argparse
+import pathlib
+import pickle
+import time
+
+import numpy as np
+from rplidar import RPLidar
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Path to save to')
+    parser.add_argument('scans_path', nargs='?', help='Scan folder')
+    args = parser.parse_args()
+
+    save_folder = args.scans_path
+    if save_folder is None:
+        save_folder = pathlib.Path('live_scans').joinpath(str(time.time()))
+    else:
+        save_folder = pathlib.Path(save_folder)
+
+    lidar = RPLidar('/dev/ttyUSB0')
+
+    save_folder.mkdir(parents=True, exist_ok=True)
+
+    for i, scan in enumerate(lidar.iter_scans()):
+        scan = np.array(scan)
+        pickle.dump(scan, open(f'{str(save_folder)}/{i:05d}.pkl', 'wb'))
+
+    lidar.stop()
+    lidar.stop_motor()
+    lidar.disconnect()
