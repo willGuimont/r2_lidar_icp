1afb92e681ccf00597543bae4b0856e5e546c4aa
William Guimont-Martin <wil9876543210@hotmail.com>
2021-11-03 20:01:22 -0400

    added main guard

--- live_lidar_tool.py
+++ live_lidar_tool.py
@@ -6,32 +6,33 @@ from rplidar import RPLidar
 
 from visualize_point_cloud import draw_point_cloud
 
-lidar = RPLidar('/dev/ttyUSB0')
-window = "lidar"
-window_size = 500
+if __name__ == '__main__':
+    lidar = RPLidar('/dev/ttyUSB0')
+    window = "lidar"
+    window_size = 500
 
-cv2.namedWindow(window)
-for i, scan in enumerate(lidar.iter_scans()):
-    img = np.zeros((window_size, window_size, 3), dtype=np.uint8)
-    scan = np.array(scan)
+    cv2.namedWindow(window)
+    for i, scan in enumerate(lidar.iter_scans()):
+        img = np.zeros((window_size, window_size, 3), dtype=np.uint8)
+        scan = np.array(scan)
 
-    qualities, angles, distances = scan[:, 0], scan[:, 1], scan[:, 2]
-    angles = np.deg2rad(angles)
+        qualities, angles, distances = scan[:, 0], scan[:, 1], scan[:, 2]
+        angles = np.deg2rad(angles)
 
-    xs = np.cos(angles) * distances
-    ys = np.sin(angles) * distances
+        xs = np.cos(angles) * distances
+        ys = np.sin(angles) * distances
 
-    pc = np.stack((xs, ys))
-    draw_point_cloud(pc, img, window_size, (255, 255, 0))
+        pc = np.stack((xs, ys))
+        draw_point_cloud(pc, img, window_size, (255, 255, 0))
 
-    cv2.imshow(window, img)
-    key = cv2.waitKey(1)
-    if key == ord(' '):
-        pickle.dump(scan, open(f'scans/{i}.pkl', 'wb'))
-    if key == ord('q') or key == 27:
-        break
+        cv2.imshow(window, img)
+        key = cv2.waitKey(1)
+        if key == ord(' '):
+            pickle.dump(scan, open(f'scans/{i}.pkl', 'wb'))
+        if key == ord('q') or key == 27:
+            break
 
-cv2.destroyAllWindows()
-lidar.stop()
-lidar.stop_motor()
-lidar.disconnect()
+    cv2.destroyAllWindows()
+    lidar.stop()
+    lidar.stop_motor()
+    lidar.disconnect()
--- simulation.py
+++ simulation.py
@@ -65,58 +65,59 @@ def point_to_window(pt):
     return (world_to_win @ point_to_homogeneous(np.array([pt])))[:2].astype(int)[:, 0]
 
 
-img = np.zeros((window_size, window_size, 3), dtype=np.uint8)
-while True:
-    img[:, :, :] = 0
+if __name__ == '__main__':
+    img = np.zeros((window_size, window_size, 3), dtype=np.uint8)
+    while True:
+        img[:, :, :] = 0
 
-    # draw walls
-    for wall in walls:
-        wall_start, wall_end = line_to_window(wall).T
-        cv2.line(img, wall_start, wall_end, color=(255, 255, 0))
-
-    # lidar
-    robot_pos_window = point_to_window(robot_pos)
-    intersections = []
-    for i in range(num_beam):
-        theta = robot_yaw + i * 2 * np.pi / num_beam
-        lidar_end_x, lidar_end_y = np.cos(theta) * lidar_range + robot_pos[0] + robot_pos[0], \
-                                   np.sin(theta) * lidar_range + robot_pos[1] + robot_pos[1]
-        line = robot_pos, np.array([lidar_end_x, lidar_end_y])
-        best_inter = None
-        smallest_distance = np.inf
+        # draw walls
         for wall in walls:
-            wall_start, wall_end = wall
-            inter = line_line_intersection(*line, wall_start, wall_end)
-            if inter is not None:
-                distance = np.linalg.norm(np.array(inter) - robot_pos)
-                if distance < smallest_distance:
-                    smallest_distance = distance
-                    best_inter = inter
+            wall_start, wall_end = line_to_window(wall).T
+            cv2.line(img, wall_start, wall_end, color=(255, 255, 0))
 
-        if best_inter is not None:
-            intersections.append(np.copy(best_inter))
+        # lidar
+        robot_pos_window = point_to_window(robot_pos)
+        intersections = []
+        for i in range(num_beam):
+            theta = robot_yaw + i * 2 * np.pi / num_beam
+            lidar_end_x, lidar_end_y = np.cos(theta) * lidar_range + robot_pos[0] + robot_pos[0], \
+                                       np.sin(theta) * lidar_range + robot_pos[1] + robot_pos[1]
+            line = robot_pos, np.array([lidar_end_x, lidar_end_y])
+            best_inter = None
+            smallest_distance = np.inf
+            for wall in walls:
+                wall_start, wall_end = wall
+                inter = line_line_intersection(*line, wall_start, wall_end)
+                if inter is not None:
+                    distance = np.linalg.norm(np.array(inter) - robot_pos)
+                    if distance < smallest_distance:
+                        smallest_distance = distance
+                        best_inter = inter
 
-        if i % 1 == 0:
-            if best_inter is None:
-                rx, ry = point_to_window(line[1])
-            else:
-                rx, ry = point_to_window(best_inter)
-            cv2.line(img, (robot_pos_window[0], robot_pos_window[1]), (rx, ry), (0, 255, 0), 1)
+            if best_inter is not None:
+                intersections.append(np.copy(best_inter))
 
-    # robot
-    cv2.circle(img, (robot_pos_window[0], robot_pos_window[1]), robot_radius, (255, 0, 255), -1)
-    robot_front_x, robot_front_y = int(np.cos(robot_yaw) * robot_radius + robot_pos_window[0]), \
-                                   int(np.sin(robot_yaw) * robot_radius + robot_pos_window[1])
-    cv2.line(img, (robot_pos_window[0], robot_pos_window[1]), (robot_front_x, robot_front_y), (255, 0, 0), 2)
+            if i % 1 == 0:
+                if best_inter is None:
+                    rx, ry = point_to_window(line[1])
+                else:
+                    rx, ry = point_to_window(best_inter)
+                cv2.line(img, (robot_pos_window[0], robot_pos_window[1]), (rx, ry), (0, 255, 0), 1)
 
-    # points
-    for inter in intersections:
-        inter = point_to_window(inter)
-        cv2.circle(img, (inter[0], inter[1]), 5, (255, 0, 127), -1)
+        # robot
+        cv2.circle(img, (robot_pos_window[0], robot_pos_window[1]), robot_radius, (255, 0, 255), -1)
+        robot_front_x, robot_front_y = int(np.cos(robot_yaw) * robot_radius + robot_pos_window[0]), \
+                                       int(np.sin(robot_yaw) * robot_radius + robot_pos_window[1])
+        cv2.line(img, (robot_pos_window[0], robot_pos_window[1]), (robot_front_x, robot_front_y), (255, 0, 0), 2)
 
-    cv2.imshow(window, img)
-    key = cv2.waitKey(1)
-    if key == ord('q') or key == 27:
-        break
+        # points
+        for inter in intersections:
+            inter = point_to_window(inter)
+            cv2.circle(img, (inter[0], inter[1]), 5, (255, 0, 127), -1)
 
-cv2.destroyAllWindows()
+        cv2.imshow(window, img)
+        key = cv2.waitKey(1)
+        if key == ord('q') or key == 27:
+            break
+
+    cv2.destroyAllWindows()
